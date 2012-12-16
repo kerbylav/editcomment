bee9f28bbef14e7b27d4db8e4933312c35850007
kerby <mail@kerby.ru>
2012-12-16 17:46:02 +0400

    english lang added

--- templates/language/english.php
+++ templates/language/english.php
@@ -1,25 +1,34 @@
 <?php
 /*-------------------------------------------------------
 *
-*   Comment Watcher
+*   kEditComment.
 *   Copyright © 2012 Alexei Lukin
 *
 *--------------------------------------------------------
 *
-*   Official site: imthinker.ru/commentwatcher
-*   Contact e-mail: kerbylav@gmail.com
+*   Official site: http://kerbystudio.ru
+*   Contact e-mail: kerby@kerbystudio.ru
 *
 ---------------------------------------------------------
 */
 
-
-/**
- * English language file for plug-in "Comment watcher
- */
 return array(
-	/**
-	 * Static pages
-	 */
+'edit_command_title'=>'Edit',
+
+'no_edit_rights'=>'Not enough rights',
+
+'edit_button_title'=>'edit',
+
+'err_max_edit_count'=>'Maximun number of editing attempts is reached',
+'err_max_edit_period'=>'Period of editing is over',
+'err_deny_with_asnwers'=>'Cannot edit comments with answers',
+'err_min_user_rating'=>'Not enough rating to edit comment',
+'err_max_comment_length'=>'Maximum length of comment is %%maxlength%% characters!',
+
+'notice_nothing_changed'=>'There were no changes',
+
+'msg_last_edited'=>'Was edited last time on ',
+
 );
 
 ?>
